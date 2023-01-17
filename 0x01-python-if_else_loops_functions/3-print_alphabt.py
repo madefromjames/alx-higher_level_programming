9fa6682127b4a5749174debcff5dbe188a9cdd11
madefromjames <madefromjames@gmail.com>
2023-01-16 17:21:06 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in 'abcdfghijklmoprstuvwxyz':
-    print("{}".format(i), end="")
+for i in 'abcdefghijklmopqrstuvwxyz':
+   if (i != 'q' and i != 'e'):
+      print("{}".format(i), end="")
