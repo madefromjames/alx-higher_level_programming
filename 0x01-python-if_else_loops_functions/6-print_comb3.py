7b0b5a4e0453bdda97878b513eaec13408b079ff
madefromjames <madefromjames@gmail.com>
2023-01-18 07:58:08 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 8):
     for j in range(i + 1, 10):
-        print("{}".format(str(i) + str(j)), end="")
-        if int(str(i) + str(j)) < 89:
-            print(", ", end="")
-print("")
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
